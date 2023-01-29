cf3e2cc3be6e9025ce913df4650b9deffc0181b6
Rishabh Mehta <rishabh.mh1@gmail.com>
2023-01-29 21:37:56 +0530

    drawing elements

--- canvas.js
+++ canvas.js
@@ -6,5 +6,25 @@ canvas.height = innerHeight
 
 const ctx = canvas.getContext('2d')
 
+ctx.fillStyle = 'rgba(255,0,0,0.5)'
 ctx.fillRect(100, 100, 100, 100)
-ctx.strokeRect(250, 100, 100, 100)
+ctx.fillStyle = 'rgba(0,0,255,0.5)'
+ctx.fillRect(400, 100, 100, 100)
+ctx.fillStyle = 'rgba(0,255,0,0.5)'
+ctx.fillRect(300, 300, 100, 100)
+
+// ctx.beginPath()
+ctx.moveTo(50, 300)
+ctx.lineTo(300, 100)
+ctx.lineTo(400, 300)
+ctx.strokeStyle = '#fa34a3'
+ctx.stroke()
+
+for (i = 0; i < 400; i++) {
+  const x = Math.random() * window.innerWidth
+  const y = Math.random() * window.innerHeight
+  ctx.beginPath()
+  ctx.strokeStyle = 'blue'
+  ctx.arc(x, y, 30, 0, Math.PI * 2, false)
+  ctx.stroke()
+}

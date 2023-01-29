45b78bf5d81426caf4151dba481a08ae1ee06598
Rishabh Mehta <rishabh.mh1@gmail.com>
2023-01-30 01:23:48 +0530

    interactivity with canvas

--- canvas.js
+++ canvas.js
@@ -34,18 +34,54 @@ const ctx = canvas.getContext('2d')
 // let dy = (Math.random() - 0.5) * 15
 // let radius = 30
 
+const mouse = {
+  x: undefined,
+  y: undefined,
+}
+
+const maxRadius = 40
+const minRadius = 2
+
+const colorArray = [
+  '#ffaa33',
+  '#99ffaa',
+  '#00ff00',
+  '#4411aa',
+  '#ff1100',
+  '#c2efb3',
+  '#97abb1',
+  '#746F72',
+  '#735F3D',
+  '#594A26',
+]
+
+window.addEventListener('mousemove', function (e) {
+  mouse.x = e.x
+  mouse.y = e.y
+  console.log(mouse)
+})
+
+window.addEventListener('resize', function () {
+  canvas.width = innerWidth
+  canvas.height = innerHeight
+
+  init()
+})
+
 function Circle(x, y, dx, dy, radius) {
   this.x = x
   this.y = y
   this.dx = dx
   this.dy = dy
   this.radius = radius
+  this.minRadius = radius
+  this.color = colorArray[Math.floor(Math.random() * colorArray.length)]
 
   this.draw = function () {
     ctx.beginPath()
     ctx.strokeStyle = 'blue'
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    ctx.stroke()
+    ctx.fillStyle = this.color
     ctx.fill()
   }
 
@@ -58,22 +94,36 @@ function Circle(x, y, dx, dy, radius) {
     this.x += this.dx
     this.y += this.dy
 
+    // interactivity
+    if (
+      mouse.x - this.x < 50 &&
+      mouse.x - this.x > -50 &&
+      mouse.y - this.y < 50 &&
+      mouse.y - this.y > -50
+    ) {
+      if (this.radius < maxRadius) {
+        this.radius += 1
+      }
+    } else if (this.radius > this.minRadius) this.radius -= 1
+
     this.draw()
   }
 }
 
-const circleArray = []
+let circleArray = []
 
-for (i = 0; i < 200; i++) {
-  let radius = 30
-  let x = Math.random() * (innerWidth - radius * 2) + radius
-  let y = Math.random() * (innerHeight - radius * 2) + radius
-  let dx = Math.random() - 0.5 * 15
-  let dy = Math.random() - 0.5 * 15
-  circleArray.push(new Circle(x, y, dx, dy, radius))
+function init() {
+  circleArray = []
+
+  for (i = 0; i < 800; i++) {
+    const radius = Math.random() * 20 + 1
+    const x = Math.random() * (innerWidth - radius * 2) + radius
+    const y = Math.random() * (innerHeight - radius * 2) + radius
+    const dx = Math.random() - 0.5
+    const dy = Math.random() - 0.5
+    circleArray.push(new Circle(x, y, dx, dy, radius))
+  }
 }
-
-console.log(circleArray)
 
 function animate() {
   requestAnimationFrame(animate)
@@ -83,5 +133,7 @@ function animate() {
     circleArray[i].update()
   }
 }
+
+init()
 
 animate()
